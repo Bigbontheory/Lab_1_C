3bd0c32839325287264aa532b2fdf60d52cfd7da
Bigbontheory <volkovsasha2007@gmail.com>
2026-03-10 21:03:18 +0300

    Update complex_field_info.h

--- complex_field_info.h
+++ complex_field_info.h
@@ -1,13 +1,16 @@
 #ifndef COMPLEX_FIELD_INFO_H_INCLUDED
 #define COMPLEX_FIELD_INFO_H_INCLUDED
+
 #include "field_info.h"
+
 typedef struct Complex
 {
     double ReZ;
     double ImZ;
 }Complex;
 
-Fieldinfo* get_complex_field_info();
+int mod_gt1_complex(const void* elem); // module greater then 1
+int mod_lt1_complex(const void* elem); // module less then 1
 
-
+const Fieldinfo* get_complex_field_info();
 #endif // COMPLEX_FIELD_INFO_H_INCLUDED
